ad5b745819895aed2b6c34733c9a617651d5b3fd
gaurav-verkada <85907198+gaurav-verkada@users.noreply.github.com>
2022-06-20 22:26:11 -0700

    add svg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: { styledComponents: true },
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,48 @@
 import type { NextPage } from "next";
 import styled from "styled-components";
+import PauseSVG from "../assets/icons/pause.svg";
+
 const Container = styled.div`
   display: flex;
   padding: 10px;
-  color: white;l
+  color: white;
+  height: 100vh;
 `;
 const Centered = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  width: 100%;
   border: 3px solid green;
+  width: 100vw;
+`;
+const Content = styled.div`
+display: flex;
+  width: 500px;
+  height 500px;
+  background-color: charcoal;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid red;
+  padding: 10px;
+  gap: 10px;
+`;
+export const Pause = styled(PauseSVG)`
+  width: 20px;
+  height: 20px;
+  fill: palevioletred;
 `;
 const Home: NextPage = () => {
   return (
     <Container>
-      <Centered>hello world</Centered>
+      <Centered>
+        <Content>
+          <div>hello world</div>
+          <div>
+            <Pause />
+          </div>
+        </Content>
+      </Centered>
     </Container>
   );
 };
